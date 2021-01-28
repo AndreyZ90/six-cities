66e0357b2e58282216f3f5816ea84df434551124
Andrey <zakharov90@gmail.com>
2021-01-28 18:57:54 +0300

    Add mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 1,
+    isFavorite: true,
+    isPremium: true,
+    previewImage: `img/apartment-01.jpg`,
+    price: 120,
+    rating: 4.9,
+    title: `Beautiful & luxurious studio at great location`,
+    type: `apartment`
+  },
+  {
+    id: 2,
+    isFavorite: true,
+    isPremium: false,
+    previewImage: `img/apartment-02.jpg`,
+    price: 99,
+    rating: 3.6,
+    title: `Wood and stone place`,
+    type: `room`
+  },
+  {
+    id: 3,
+    isFavorite: false,
+    isPremium: true,
+    previewImage: `img/apartment-03.jpg`,
+    price: 1000,
+    rating: 3.4,
+    title: `Canal View Prinsengracht`,
+    type: `house`
+  },
+  {
+    id: 4,
+    isFavorite: false,
+    isPremium: false,
+    previewImage: `img/apartment-01.jpg`,
+    price: 298,
+    rating: 0.9,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `hotel`
+  }
+];
