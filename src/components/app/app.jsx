c1bf061097158495410551fd32328b108e4a630e
Andrey <zakharov90@gmail.com>
2021-01-28 20:56:21 +0300

    Update App component (change props) and update test

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,14 +3,25 @@ import PropTypes from 'prop-types';
 
 import Main from '@/components/main/main';
 
-const App = (props) => {
-  const {titlesOffers, onTitleClick} = props;
+import {HouseType} from '@/helpers/const';
 
-  return <Main titlesOffers={titlesOffers} onTitleClick={onTitleClick} />;
+const App = (props) => {
+  const {offers, onTitleClick} = props;
+
+  return <Main offers={offers} onTitleClick={onTitleClick} />;
 };
 
 App.propTypes = {
-  titlesOffers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  offers: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    previewImage: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(Object.keys(HouseType)).isRequired,
+  })).isRequired,
   onTitleClick: PropTypes.func.isRequired
 };
 
