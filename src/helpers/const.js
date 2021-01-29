cea3408e2346a1fe424c8e77e75392e10a8825aa
Andrey <zakharov90@gmail.com>
2021-01-29 21:36:21 +0300

    Update helpers (add const MapPin, MapPinActive)

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -5,4 +5,14 @@ const HouseType = {
   hotel: `Hotel`
 };
 
-export {HouseType};
+const MapPin = {
+  src: `/img/pin.svg`,
+  size: [30, 40]
+};
+
+const MapPinActive = {
+  src: `/img/pin-active.svg`,
+  size: [30, 40]
+};
+
+export {HouseType, MapPin, MapPinActive};
