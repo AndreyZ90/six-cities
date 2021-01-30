2f9bef7ec435e9ffb55e9f618625043c16996a1a
Andrey <zakharov90@gmail.com>
2021-01-30 15:45:49 +0300

    Update helpers (add new fn - getGeoCoords) and move test in dir _tests_

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -6,4 +6,20 @@ const convertRatingToStyle = (rating) => {
   return rating < 1 ? step : Math.round(rating) * step;
 };
 
-export {convertRatingToStyle};
+const getGeoCoords = (data) => {
+  if (data instanceof Array) {
+    return data.map(({location}) => [location.latitude, location.longitude]);
+  }
+
+  if (!(data instanceof Array) && data instanceof Object) {
+    if (!data) {
+      return [];
+    }
+
+    return [data.location.latitude, data.location.longitude];
+  }
+
+  return [];
+};
+
+export {convertRatingToStyle, getGeoCoords};
