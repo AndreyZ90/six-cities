0ecea41d9d290371c39e27f6bbdb3c5c817a78c8
Andrey <zakharov90@gmail.com>
2021-01-30 11:04:07 +0300

    Update index.js (add mocks - reviews)

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import {render} from 'react-dom';
 import App from '@/components/app/app';
 
 import offers from '@/mocks/offers';
+import reviews from '@/mocks/reviews';
 
 const onTitleClick = () => {};
 
 render(
     <App
       offers={offers}
+      reviews={reviews}
       onTitleClick={onTitleClick}
     />,
     document.getElementById(`root`)
