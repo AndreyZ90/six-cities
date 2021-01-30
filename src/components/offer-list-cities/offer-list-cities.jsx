bd443cd506ae8e2fad234a6383645ad5009447ff
Andrey <zakharov90@gmail.com>
2021-01-30 23:04:45 +0300

    Add OfferListCities component and test

---
+++ src/components/offer-list-cities/offer-list-cities.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import OfferList from '@/components/offer-list/offer-list';
+
+import {ClassPrefix, HouseType} from '@/helpers/const';
+
+const OfferListCities = (props) => {
+  return <OfferList className="tabs__content cities__places-list" classPrefix={ClassPrefix.CITY} {...props} />;
+};
+
+OfferListCities.propTypes = {
+  offers: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    previewImage: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(Object.keys(HouseType)).isRequired
+  })).isRequired,
+  onTitleClick: PropTypes.func.isRequired,
+  onActiveCardChange: PropTypes.func
+};
+
+export default OfferListCities;
---
+++ src/components/offer-list-cities/offer-list-cities.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+import OfferListCities from '@/components/offer-list-cities/offer-list-cities';
+
+const offers = [
+  {
+    id: 1,
+    isFavorite: true,
+    isPremium: true,
+    previewImage: `img/apartment-01.jpg`,
+    price: 120,
+    rating: 4.9,
+    title: `Beautiful & luxurious studio at great location`,
+    type: `apartment`
+  },
+  {
+    id: 2,
+    isFavorite: true,
+    isPremium: false,
+    previewImage: `img/apartment-02.jpg`,
+    price: 99,
+    rating: 3.6,
+    title: `Wood and stone place`,
+    type: `room`
+  }
+];
+
+describe(`OfferListCities component snapshot`, () => {
+  test(`Should correctly render OfferListCities component`, () => {
+    const tree = TestRenderer.create(
+        <OfferListCities
+          offers={offers}
+          onTitleClick={() => {}}
+        />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
