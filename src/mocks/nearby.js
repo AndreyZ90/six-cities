8262b0362af49a0f83d40f97a049ca55688c5a12
Andrey <zakharov90@gmail.com>
2021-01-30 15:47:55 +0300

    Update mocks (add new data - nearby)

---
+++ src/mocks/nearby.js
@@ -0,0 +1,104 @@
+export default [
+  {
+    id: 2,
+    isFavorite: true,
+    isPremium: false,
+    previewImage: `img/apartment-02.jpg`,
+    price: 99,
+    rating: 3.6,
+    title: `Wood and stone place`,
+    type: `room`,
+    images: [`img/apartment-03.jpg`, `img/apartment-02.jpg`, `img/apartment-01.jpg`, `img/studio-01.jpg`, `img/apartment-01.jpg`, `img/room.jpg`],
+    berdrooms: 1,
+    maxAdults: 2,
+    goods: [`Washing machine`, `Coffee machine`, `Dishwasher`],
+    host: {
+      avatarUrl: `img/avatar-angelina.jpg`,
+      id: 2,
+      isPro: false,
+      name: `Nadezhda`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Moscow.`,
+    location: {
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
+      zoom: 8
+    },
+    city: {
+      location: {
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
+      },
+      name: `Amsterdam`
+    }
+  },
+  {
+    id: 3,
+    isFavorite: false,
+    isPremium: true,
+    previewImage: `img/apartment-03.jpg`,
+    price: 1000,
+    rating: 3.4,
+    title: `Canal View Prinsengracht`,
+    type: `house`,
+    images: [`img/apartment-02.jpg`, `img/apartment-01.jpg`, `img/room.jpg`, `img/studio-01.jpg`, `img/apartment-01.jpg`, `img/apartment-03.jpg`],
+    berdrooms: 2,
+    maxAdults: 5,
+    goods: [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    host: {
+      avatarUrl: `img/avatar-angelina.jpg`,
+      id: 3,
+      isPro: false,
+      name: `Polina`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Berlin.`,
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
+      zoom: 8
+    },
+    city: {
+      location: {
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
+      },
+      name: `Amsterdam`
+    }
+  },
+  {
+    id: 4,
+    isFavorite: false,
+    isPremium: false,
+    previewImage: `img/apartment-01.jpg`,
+    price: 298,
+    rating: 0.9,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `hotel`,
+    images: [`img/apartment-01.jpg`, `img/room.jpg`, `img/studio-01.jpg`, `img/apartment-03.jpg`, `img/apartment-01.jpg`, `img/apartment-02.jpg`],
+    berdrooms: 2,
+    maxAdults: 2,
+    goods: [`Kitchen`, `Washing machine`],
+    host: {
+      avatarUrl: `img/avatar-angelina.jpg`,
+      id: 4,
+      isPro: true,
+      name: `Jesica`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of London.`,
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 8
+    },
+    city: {
+      location: {
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
+      },
+      name: `Amsterdam`
+    }
+  }
+];
