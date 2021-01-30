8165785c3868f45148b1644a5bbd68906d8d42f1
Andrey <zakharov90@gmail.com>
2021-01-30 23:01:36 +0300

    Add OfferCardCities component and test

---
+++ src/components/offer-card-cities/offer-card-cities.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import OfferCard from '@/components/offer-card/offer-card';
+
+import {HouseType} from '@/helpers/const';
+
+const OfferCardCities = (props) => {
+  return <OfferCard className="cities__place-card" {...props} />;
+};
+
+OfferCardCities.propTypes = {
+  offer: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    previewImage: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(Object.keys(HouseType)).isRequired
+  }).isRequired,
+  onTitleClick: PropTypes.func.isRequired,
+  onActiveCardChange: PropTypes.func
+};
+
+export default OfferCardCities;
