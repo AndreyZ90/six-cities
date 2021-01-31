2751f398d7288668a73a263b0495a9319eba25dc
Andrey <zakharov90@gmail.com>
2021-01-31 18:34:43 +0300

    Add PreviewImage component and test

---
+++ src/components/preview-image/preview-image.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PreviewImage = (props) => {
+  const {src} = props;
+
+  return (
+    <div className="place-card__image-wrapper">
+      <a>
+        <img className="place-card__image" src={`/${src}`} width={260} height={200} alt="Place image" />
+      </a>
+    </div>
+  );
+};
+
+PreviewImage.propTypes = {
+  src: PropTypes.string.isRequired
+};
+
+export default PreviewImage;
