2393ac86895b6519e42af464f85122e241727038
Andrey <zakharov90@gmail.com>
2021-01-31 18:43:45 +0300

    Refactored OfferCard component (replace markup on components) and update test

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import PremiumMarkCard from '@/components/premium-mark-card/premium-mark-card';
+import PreviewImage from '@/components/preview-image/preview-image';
+import PriceCard from '@/components/price-card/price-card';
+import BookmarkButtonCard from '@/components/bookmark-button-card/bookmark-button-card';
+import RatingCard from '@/components/rating-card/rating-card';
+
 import {HouseType} from '@/helpers/const';
-import {convertRatingToStyle} from '@/helpers/common';
 
 const OfferCard = (props) => {
   const {
@@ -16,47 +21,25 @@ const OfferCard = (props) => {
       title,
       type,
     },
-    className,
     onTitleClick,
+    className,
     onActiveCardChange,
   } = props;
 
-  const activeClass = isFavorite ? `place-card__bookmark-button--active` : ``;
-
   return (
-    <article className={`${className} place-card`}
+    <article
+      className={`${className} place-card`}
       onMouseEnter={() => onActiveCardChange(id)}
       onMouseLeave={() => onActiveCardChange(-1)}
     >
-      {isPremium &&
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
-      }
-      <div className="place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src={`/${previewImage}`} width={260} height={200} alt="Place image" />
-        </a>
-      </div>
+      {isPremium && <PremiumMarkCard />}
+      <PreviewImage src={previewImage} />
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">€{price}</b>
-            <span className="place-card__price-text">/&nbsp;night</span>
-          </div>
-          <button className={`place-card__bookmark-button button ${activeClass}`} type="button">
-            <svg className="place-card__bookmark-icon" width={18} height={19}>
-              <use xlinkHref="#icon-bookmark" />
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
+          <PriceCard price={price} />
+          <BookmarkButtonCard isActive={isFavorite} />
         </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={{width: `${convertRatingToStyle(rating)}%`}} />
-            <span className="visually-hidden">Rating</span>
-          </div>
-        </div>
+        <RatingCard rating={rating} />
         <h2 className="place-card__name" onClick={onTitleClick}>
           <a href="#">{title}</a>
         </h2>
