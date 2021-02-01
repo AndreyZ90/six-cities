36d8480a5bbe0a29dc676a0ed51de5bd052e77e8
Andrey <zakharov90@gmail.com>
2021-02-01 20:27:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import App from '@/components/app/app';
 
-import offers from '@/mocks/offers';
+import reducer from '@/reducers/reducer';
+import {getOffers} from '@/actions/action-creator';
+
 import reviews from '@/mocks/reviews';
 import nearby from '@/mocks/nearby';
 
 const onTitleClick = () => {};
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.dispatch(getOffers());
+
 render(
-    <App
-      offers={offers}
-      reviews={reviews}
-      nearby={nearby}
-      onTitleClick={onTitleClick}
-    />,
+    <Provider store={store}>
+      <App
+        reviews={reviews}
+        nearby={nearby}
+        onTitleClick={onTitleClick}
+      />
+    </Provider>,
     document.getElementById(`root`)
 );
