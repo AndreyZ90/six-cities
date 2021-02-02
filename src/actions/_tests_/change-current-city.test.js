1c76478d682555aa6b883a240362b22c36a96f07
Andrey <zakharov90@gmail.com>
2021-02-02 14:11:34 +0300

    Update actions (add new action - changeCurrentSort) and test

--- src/actions/_tests_/change-current-city.test.js
+++ src/actions/_tests_/change-current-city.test.js
@@ -1,14 +1,7 @@
 import {changeCurrentCity} from '@/actions/action-creator';
 
 describe(`ActionCreator - changeCurrentCity tests`, () => {
-  test(`Should return object with right type (without payload)`, () => {
-    expect(changeCurrentCity()).toEqual({
-      type: `CHANGE_CURRENT_CITY`,
-      payload: undefined
-    });
-  });
-
-  test(`Should return object with right type (with payload)`, () => {
+  test(`Should return object with right type`, () => {
     expect(changeCurrentCity(`Moscow`)).toEqual({
       type: `CHANGE_CURRENT_CITY`,
       payload: `Moscow`
