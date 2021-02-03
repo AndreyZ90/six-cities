9a4a21b1fa2e32d73747f33b29b00f7c56137d6f
Andrey <zakharov90@gmail.com>
2021-02-03 18:58:57 +0300

    Update Map component (add new fn for render markers) and test

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,36 +1,61 @@
-import React, {createRef, PureComponent} from 'react';
+import React, {Component, createRef} from 'react';
 import PropTypes from 'prop-types';
-import leaflet, {layerGroup} from 'leaflet';
+import leaflet from 'leaflet';
 
-import {ClassPrefix, MapPin} from '@/helpers/const';
+import {ClassPrefix, MapPin, MapPinActive} from '@/helpers/const';
 
-export default class Map extends PureComponent {
+export default class Map extends Component {
   constructor(props) {
     super(props);
 
     this._mapRef = createRef();
+
     this._leaflet = null;
     this._icon = null;
+    this._activeIcon = null;
     this._titleLayer = null;
-    this._markersLayers = null;
+    this._layerGroupMarkers = null;
+  }
+
+  _updateLayerGroupMarkers(geoCoords, activeGeoCoords) {
+    if (this._layerGroupMarkers) {
+      this._layerGroupMarkers.clearLayers();
+    }
+
+    geoCoords.map((coords) => this._layerGroupMarkers.addLayer(leaflet.marker(coords, {icon: this._icon})));
+
+    if (activeGeoCoords.length > 0) {
+      const activeMarkerLayer = leaflet.marker(activeGeoCoords, {icon: this._activeIcon});
+      this._layerGroupMarkers.addLayer(activeMarkerLayer);
+    }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.activeGeoCoords === this.props.activeGeoCoords) {
+      return false;
+    }
+    this._updateLayerGroupMarkers(nextProps.geoCoords, nextProps.activeGeoCoords);
+    return true;
   }
 
   componentDidMount() {
-    const {geoCoords, center, zoom} = this.props;
+    const {geoCoords, activeGeoCoords, center, zoom} = this.props;
 
     this._icon = leaflet.icon({iconUrl: MapPin.src, size: MapPin.size});
+    this._activeIcon = leaflet.icon({iconUrl: MapPinActive.src, size: MapPinActive.size});
 
     this._titleLayer = leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
       attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
     });
 
-    this._markersLayers = geoCoords.map((coords) => leaflet.marker(coords, {icon: this._icon}));
+    this._layerGroupMarkers = leaflet.layerGroup();
+    this._updateLayerGroupMarkers(geoCoords, activeGeoCoords);
 
     this._leaflet = leaflet.map(this._mapRef.current, {
       center,
       zoom,
       zoomControl: false,
-      layers: [this._titleLayer, layerGroup(this._markersLayers)]
+      layers: [this._titleLayer, this._layerGroupMarkers]
     });
   }
 
@@ -47,6 +72,7 @@ Map.defaultProps = {
 
 Map.propTypes = {
   geoCoords: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
+  activeGeoCoords: PropTypes.array.isRequired,
   center: PropTypes.arrayOf(PropTypes.number).isRequired,
   zoom: PropTypes.number.isRequired,
   classPrefix: PropTypes.oneOf(Object.values(ClassPrefix))
