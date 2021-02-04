4584a485cf49dd062c83b9e775954e646034154b
Andrey <zakharov90@gmail.com>
2021-02-04 17:54:18 +0300

    Update SortItem component (add new props) and test

--- src/components/sort-item/sort-item.jsx
+++ src/components/sort-item/sort-item.jsx
@@ -2,17 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SortItem = (props) => {
-  const {item, isActive, onCurrentSortChange} = props;
+  const {item, isActive, onCurrentSortChange, onFlagChange} = props;
 
   const activeClass = isActive ? `places__option--active` : ``;
 
-  return <li className={`places__option ${activeClass}`} tabIndex={0} onClick={() => onCurrentSortChange(item)}>{item}</li>;
+  return (
+    <li
+      className={`places__option ${activeClass}`}
+      tabIndex={0}
+      onClick={() => {
+        onCurrentSortChange(item);
+        onFlagChange();
+      }}>
+      {item}
+    </li>);
 };
 
 SortItem.propTypes = {
   item: PropTypes.string.isRequired,
   isActive: PropTypes.bool.isRequired,
-  onCurrentSortChange: PropTypes.func.isRequired
+  onCurrentSortChange: PropTypes.func.isRequired,
+  onFlagChange: PropTypes.func.isRequired
 };
 
 export default SortItem;
