3797dd761352c274590eb34f4a7cd132229dd758
Andrey <zakharov90@gmail.com>
2021-02-05 18:09:56 +0300

    Update index.js (add redux-thunk, api)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 import App from '@/containers/app/app';
 
-import reducer from '@/reducers/reducer';
-import {getOffers} from '@/actions/action-creator';
+import reducer from '@/store/reducers/reducer';
+import Operation from '@/store/actions/operation';
+import createApi from '@/services/api';
 
 import reviews from '@/mocks/reviews';
 import nearby from '@/mocks/nearby';
 
 const onTitleClick = () => {};
 
+const api = createApi();
+
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)))
 );
 
-store.dispatch(getOffers());
+store.dispatch(Operation.fetchOffersRequest());
 
 render(
     <Provider store={store}>
