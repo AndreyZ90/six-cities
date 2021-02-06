05dae785193c7c9c83c279fb6471757e4cd0583e
Andrey <zakharov90@gmail.com>
2021-02-06 13:36:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
 
 import App from '@/containers/app/app';
 
-import reducer from '@/store/reducers/reducer';
+import store from '@/store/store';
+import ActionCreator from '@/store/actions/creator';
 import Operation from '@/store/actions/operation';
-import createApi from '@/services/api';
 
 import reviews from '@/mocks/reviews';
 import nearby from '@/mocks/nearby';
 
 const onTitleClick = () => {};
 
-const api = createApi();
+store.dispatch(Operation.fetchOffersRequest())
+  .then((offers) => {
+    const cities = [...new Set(offers.map(({city}) => city.name))];
+    store.dispatch(ActionCreator.setCities(cities));
 
-const store = createStore(
-    reducer,
-    composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)))
-);
-
-store.dispatch(Operation.fetchOffersRequest());
+    return cities;
+  })
+  .then((cities) => store.dispatch(ActionCreator.setCurrentCity(cities[0])))
+  .then(() => store.dispatch(ActionCreator.fetchDataSuccess()));
 
 render(
     <Provider store={store}>
