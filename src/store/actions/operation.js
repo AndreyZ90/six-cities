75399de4d9be0bf28440dd701cfc55e90b52e366
Andrey <zakharov90@gmail.com>
2021-02-06 13:26:21 +0300

    Update operations (fetchOfferRequest)

--- src/store/actions/operation.js
+++ src/store/actions/operation.js
@@ -1,3 +1,4 @@
+
 import ActionCreator from '@/store/actions/creator';
 import Adapter from '@/helpers/adapter';
 
@@ -8,13 +9,10 @@ const Operation = {
     return api.get(`hotels`)
       .then(({data}) => {
         const offers = Adapter.offers(data);
-        const cities = [...new Set(offers.map(({city}) => city.name))];
 
         dispatch(ActionCreator.fetchOffersSuccess(offers));
-        dispatch(ActionCreator.getCities(cities));
-        dispatch(ActionCreator.setCurrentCity(cities[0]));
 
-        dispatch(ActionCreator.fetchDataSuccess());
+        return offers;
       });
   }
 };
