8e0743a406fe207df12ff5796f1c7e88cd9be1bd
Andrey <zakharov90@gmail.com>
2021-02-10 15:01:35 +0300

    Update Sort and SortItem (add Link and delete unused props) and test

--- src/components/sort/sort.test.js
+++ src/components/sort/sort.test.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import {Sort} from '@/components/sort/sort';
 
 const currentSort = `Popular`;
+const currentCity = `Amsterdam`;
 
 describe(`Sort component snapshot`, () => {
   test(`Should correctly render Sort component`, () => {
     const tree = TestRenderer.create(
-        <Sort
-          currentSort={currentSort}
-          isActiveFlag={false}
-          onFlagChange={() => {}}
-          onCurrentSortChange={() => {}} />
+        <Router>
+          <Sort
+            currentSort={currentSort}
+            isActiveFlag={false}
+            currentCity={currentCity}
+            onFlagChange={() => {}}
+          />
+        </Router>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
