ea43f794204cbdc2387ec80dbbc830c29ac9dc96
Andrey <zakharov90@gmail.com>
2021-02-11 19:17:06 +0300

    Update BookmarkButton component (add feature - add/remove favorite list)

--- src/components/bookmark-button/bookmark-button.jsx
+++ src/components/bookmark-button/bookmark-button.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {ClassPrefix} from '@/helpers/const';
+import {AppRoute, AuthStatus, ClassPrefix} from '@/helpers/const';
 
 const BookmarkButton = (props) => {
-  const {isActive, classPrefix, size: [width, height]} = props;
+  const {
+    isActive,
+    onBookmarkButtonClick,
+    history,
+    authStatus,
+    id,
+    classPrefix,
+    size: [width, height]
+  } = props;
 
   const activeClass = isActive ? `${classPrefix}__bookmark-button--active` : ``;
+  const statusFlag = isActive ? 0 : 1;
 
   return (
-    <button className={`${classPrefix}__bookmark-button button ${activeClass}`} type="button">
+    <button
+      className={`${classPrefix}__bookmark-button button ${activeClass}`}
+      type="button"
+      onClick={() => authStatus === AuthStatus.AUTH ? onBookmarkButtonClick(id, statusFlag) : history.push(AppRoute.LOGIN)}
+    >
       <svg className={`${classPrefix}__bookmark-icon`} width={width} height={height}>
         <use xlinkHref="#icon-bookmark" />
       </svg>
@@ -25,6 +38,12 @@ BookmarkButton.defaultProps = {
 
 BookmarkButton.propTypes = {
   isActive: PropTypes.bool.isRequired,
+  onBookmarkButtonClick: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  authStatus: PropTypes.oneOf(Object.values(AuthStatus)).isRequired,
+  id: PropTypes.number.isRequired,
   classPrefix: PropTypes.oneOf(Object.values(ClassPrefix)),
   size: PropTypes.arrayOf(PropTypes.number)
 };
--- src/components/bookmark-button/bookmark-button.test.js
+++ src/components/bookmark-button/bookmark-button.test.js
@@ -5,33 +5,77 @@ import BookmarkButton from '@/components/bookmark-button/bookmark-button';
 
 import {ClassPrefix} from '@/helpers/const';
 
+const history = {
+  push: () => {}
+};
+
 describe(`BookmarkButton component snapshot`, () => {
   test(`Should correctly render BookmarkButton component (default)`, () => {
-    const tree = TestRenderer.create(<BookmarkButton isActive={true} />).toJSON();
+    const tree = TestRenderer.create(
+        <BookmarkButton
+          isActive={true}
+          id={10}
+          authStatus={`NO_AUTH`}
+          onBookmarkButtonClick={() => {}}
+          history={history}
+        />
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test(`Should correctly render BookmarkButton component (with prefix CARD)`, () => {
     const tree = TestRenderer.create(
-        <BookmarkButton isActive={true} classPrefix={ClassPrefix.CARD} size={[18, 19]} />
+        <BookmarkButton
+          isActive={true}
+          id={10}
+          authStatus={`AUTH`}
+          onBookmarkButtonClick={() => {}}
+          history={history}
+          classPrefix={ClassPrefix.CARD}
+          size={[18, 19]}
+        />
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test(`Should correctly render BookmarkButton component (with prefix DETAILS)`, () => {
     const tree = TestRenderer.create(
-        <BookmarkButton isActive={true} classPrefix={ClassPrefix.DETAILS} size={[31, 33]} />
+        <BookmarkButton
+          isActive={true}
+          id={10}
+          authStatus={`AUTH`}
+          onBookmarkButtonClick={() => {}}
+          history={history}
+          classPrefix={ClassPrefix.DETAILS}
+          size={[31, 33]}
+        />
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test(`Should correctly render BookmarkButton component (active)`, () => {
-    const tree = TestRenderer.create(<BookmarkButton isActive={true} />).toJSON();
+    const tree = TestRenderer.create(
+        <BookmarkButton
+          isActive={true}
+          id={10}
+          authStatus={`AUTH`}
+          onBookmarkButtonClick={() => {}}
+          history={history}
+        />
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test(`Should correctly render BookmarkButton component (not active)`, () => {
-    const tree = TestRenderer.create(<BookmarkButton isActive={false} />).toJSON();
+    const tree = TestRenderer.create(
+        <BookmarkButton
+          isActive={false}
+          id={10}
+          authStatus={`AUTH`}
+          onBookmarkButtonClick={() => {}}
+          history={history}
+        />
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
