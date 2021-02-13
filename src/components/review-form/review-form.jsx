b54922988f283b7e0ad0fc49c908fdda22961df2
Andrey <zakharov90@gmail.com>
2021-02-13 18:19:20 +0300

    Update ReviewForm and test

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -1,50 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ReviewForm = () => {
+import withValidationForm from '@/HOCs/with-validation-form/with-validation-form';
+
+import ReviewFormStarList from '@/components/review-form-star-list/review-form-star-list';
+import ReviewFormTextArea from '@/components/review-form-textarea/review-form-textarea';
+import ReviewFormSubmit from '@/components/review-form-submit/review-form-submit';
+
+const ReviewForm = (props) => {
+  const {
+    id,
+    review,
+    rating,
+    isValid,
+    onValueChange,
+    onFormSubmit,
+    onFormClear
+  } = props;
+
   return (
-    <form className="reviews__form form" action="#" method="post">
+    <form
+      className="reviews__form form"
+      action="#"
+      method="post"
+      onSubmit={(evt) => {
+        evt.preventDefault();
+        onFormSubmit(id, {comment: review, rating}).then(() => onFormClear());
+      }}
+    >
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" defaultValue={5} id="5-stars" type="radio" />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input className="form__rating-input visually-hidden" name="rating" defaultValue={4} id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input className="form__rating-input visually-hidden" name="rating" defaultValue={3} id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input className="form__rating-input visually-hidden" name="rating" defaultValue={2} id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input className="form__rating-input visually-hidden" name="rating" defaultValue={1} id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-      </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={``} />
-      <div className="reviews__button-wrapper">
-        <p className="reviews__help">
-        To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-        </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-      </div>
+      <ReviewFormStarList currentRating={rating} onRatingChange={onValueChange} />
+      <ReviewFormTextArea text={review} onTextChange={onValueChange} />
+      <ReviewFormSubmit isValid={isValid} />
     </form>
   );
 };
 
-export default ReviewForm;
+ReviewForm.propTypes = {
+  id: PropTypes.number.isRequired,
+  review: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  isValid: PropTypes.bool.isRequired,
+  onValueChange: PropTypes.func.isRequired,
+  onFormSubmit: PropTypes.func.isRequired,
+  onFormClear: PropTypes.func.isRequired
+};
+
+export {ReviewForm};
+export default withValidationForm(ReviewForm);
--- src/components/review-form/review-form.test.js
+++ src/components/review-form/review-form.test.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 
-import ReviewForm from '@/components/review-form/review-form';
+import {ReviewForm} from '@/components/review-form/review-form';
 
 describe(`ReviewForm component snapshot`, () => {
   test(`Should correctly render ReviewForm component`, () => {
-    const tree = TestRenderer.create(<ReviewForm/>).toJSON();
+    const tree = TestRenderer.create(
+        <ReviewForm
+          id={1}
+          review={`Hello world`}
+          rating={5}
+          isValid={false}
+          onValueChange={() => {}}
+          onFormSubmit={() => {}}
+          onFormClear={() => {}}
+        />
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
