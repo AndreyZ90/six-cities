69c8806550843d7a21b323201b5d5a8fbbc13c9b
Andrey <zakharov90@gmail.com>
2021-03-28 16:59:20 +0300

    Update SignIn component and test

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,61 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from '@/containers/header/header';
+import SignInForm from '../sign-in-form/sign-in-form';
+import SignInInput from '../sign-in-input/sign-in-input';
+import SignInLocation from '@/components/sign-in-location/sign-in-location';
 
 const SignIn = (props) => {
-  const {email, password, onDataChange, onButtonSubmit} = props;
+  const {email, password, onDataChange, city, onButtonSubmit} = props;
 
   return (
-    <div className="page page--gray page--login">
-      <Header />
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <form
-              className="login__form form"
-              action="#"
-              method="post"
-              onSubmit={(evt) => {
-                evt.preventDefault();
-                onButtonSubmit(email, password);
-              }}>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input
-                  className="login__input form__input"
-                  type="email" name="email"
-                  placeholder="Email"
-                  required
-                  value={email}
-                  onChange={(evt) => onDataChange(evt.target.name, evt.target.value)}
-                />
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input
-                  className="login__input form__input"
-                  type="password" name="password"
-                  placeholder="Password"
-                  value={password}
-                  required
-                  onChange={(evt) => onDataChange(evt.target.name, evt.target.value)}
-                />
-              </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
-            </form>
-          </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="#">
-                <span>Amsterdam</span>
-              </a>
-            </div>
-          </section>
-        </div>
-      </main>
-    </div>
+    <main className="page__main page__main--login">
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <SignInForm email={email} password={password} onButtonSubmit={onButtonSubmit}>
+            <SignInInput type="email" label="E-mail" onDataChange={onDataChange} />
+            <SignInInput type="password" label="Password" onDataChange={onDataChange} />
+          </SignInForm>
+        </section>
+        <SignInLocation city={city} />
+      </div>
+    </main>
   );
 };
 
@@ -63,6 +28,7 @@ SignIn.propTypes = {
   email: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
   onDataChange: PropTypes.func.isRequired,
+  city: PropTypes.string.isRequired,
   onButtonSubmit: PropTypes.func.isRequired
 };
 
